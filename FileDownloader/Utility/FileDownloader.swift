87281b412f2b163bab1f9d3d99fd589ee6ee4082
Yuta Kawabe <yuyu3165@gmail.com>
2020-03-26 12:51:40 +0900

    create FileDownloader

--- FileDownloader/Utility/FileDownloader.swift
+++ FileDownloader/Utility/FileDownloader.swift
@@ -7,3 +7,63 @@
 //
 
 import Foundation
+import RxCocoa
+import RxSwift
+
+enum DownloadState {
+    case downloading(Float)
+    case done(URL)
+    case error(Error)
+}
+
+final class FileDownloader: NSObject {
+    
+    private var state = PublishSubject<DownloadState>()
+    private var filename = ""
+    private var task: URLSessionDownloadTask!
+    
+    func download(_ url: URL, name: String? = nil) -> Observable<DownloadState> {
+        filename = name ?? url.lastPathComponent
+        return Observable.create { [weak self] observer in
+            guard let self = self else {
+                return Disposables.create()
+            }
+            let disposable = self.state.bind(to: observer)
+            self.startDownload(url)
+            return disposable
+        }
+    }
+    
+    func cancel() {
+        task.cancel()
+    }
+}
+
+extension FileDownloader {
+    
+    private func startDownload(_ url: URL) {
+        let config = URLSessionConfiguration.default
+        let session = URLSession(configuration: config, delegate: self, delegateQueue: .main)
+        let request = URLRequest(url: url)
+        task = session.downloadTask(with: request)
+        task.resume()
+    }
+}
+
+extension FileDownloader: URLSessionDownloadDelegate {
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
+        state.onNext(.done(location))
+    }
+    
+    func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
+        let current = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
+        state.onNext(.downloading(current))
+    }
+    
+    func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
+        if let error = error {
+            state.onNext(.error(error))
+        }
+    }
+}
