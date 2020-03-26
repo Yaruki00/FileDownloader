ae9a17e6caf592598b99646333e75f26870ed9ac
Yuta Kawabe <yuyu3165@gmail.com>
2020-03-26 17:55:53 +0900

    add comment

--- FileDownloader/App/Presentation/ViewController.swift
+++ FileDownloader/App/Presentation/ViewController.swift
@@ -34,16 +34,22 @@ extension ViewController {
             .bind(to: button.rx.isEnabled)
             .disposed(by: disposeBag)
         
+        // ボタンが押された
         button.rx.tap
+            // テキストフィールドの文字列をURLに変換
             .map { [weak self] _ -> URL? in
                 let text = self?.textField.text ?? ""
                 return URL(string: text)
             }
+            // オプショナルのアンラップ
             .flatMap { $0.flatMap(Observable.just) ?? Observable.empty() }
+            // URLをもとにダウンロード
             .flatMap { [weak self] url -> Observable<DownloadState> in
                 return self?.fileDownloader.download(url) ?? .error(NSError(domain: "", code: 0, userInfo: nil))
             }
+            // 前回と同じ状態なら無視
             .distinctUntilChanged()
+            // ダウンロードの状態をもとにラベルに表示する文字列生成
             .map { state -> String in
                 switch state {
                 case .downloading(let progress):
@@ -54,6 +60,7 @@ extension ViewController {
                     return "error occurred\n\(error.localizedDescription)"
                 }
             }
+            // ラベルに流す
             .bind(to: label.rx.text)
             .disposed(by: disposeBag)
     }
--- FileDownloader/Utility/FileDownloader.swift
+++ FileDownloader/Utility/FileDownloader.swift
@@ -10,9 +10,13 @@ import Foundation
 import RxCocoa
 import RxSwift
 
+/// ダウンロードの状態
 enum DownloadState: Equatable {
+    /// ダウンロード中(進捗)
     case downloading(Float)
+    /// 完了(保存先URL)
     case done(URL)
+    /// 失敗(エラー詳細)
     case error(Error)
     
     static func == (lhs: DownloadState, rhs: DownloadState) -> Bool {
@@ -31,22 +35,27 @@ enum DownloadState: Equatable {
 
 final class FileDownloader: NSObject {
     
+    // ダウンロードの状態を流す用
+    // 繰り返し使う想定なのでPublishRelayにして、ObservableでいうonErrorは使わないようにする
     private var state = PublishRelay<DownloadState>()
     private var filename = ""
     private var task: URLSessionDownloadTask!
     
+    /// ダウンロード開始
     func download(_ url: URL, name: String? = nil) -> Observable<DownloadState> {
         filename = name ?? url.lastPathComponent
         return Observable.create { [weak self] observer in
             guard let self = self else {
                 return Disposables.create()
             }
+            // 関数呼び出されるたびにbindしてるけど大丈夫？教えてエラい人！
             let disposable = self.state.bind(to: observer)
             self.startDownload(url)
             return disposable
         }
     }
     
+    /// ダウンロード中断
     func cancel() {
         task.cancel()
     }
@@ -65,15 +74,18 @@ extension FileDownloader {
 
 extension FileDownloader: URLSessionDownloadDelegate {
     
+    // ダウンロードが完了した時に呼ばれる
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didFinishDownloadingTo location: URL) {
         state.accept(.done(location))
     }
     
+    // ダウンロードの進捗が変化した時に呼ばれる
     func urlSession(_ session: URLSession, downloadTask: URLSessionDownloadTask, didWriteData bytesWritten: Int64, totalBytesWritten: Int64, totalBytesExpectedToWrite: Int64) {
         let current = Float(totalBytesWritten) / Float(totalBytesExpectedToWrite)
         state.accept(.downloading(current))
     }
     
+    // ダウンロードが失敗した時に呼ばれる
     func urlSession(_ session: URLSession, task: URLSessionTask, didCompleteWithError error: Error?) {
         if let error = error {
             state.accept(.error(error))
